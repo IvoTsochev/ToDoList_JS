b129d0bf20d0c1a405af48eed259ece9763453aa
Ivaylo Tsochev <itsochev@godaddy.com>
2019-12-31 17:14:53 +0200

    configuring the adding task to the taks list

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-document.body.addEventListener('click', deleteItem);
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-//deleting the list element
-function deleteItem(e) {
-    if ( e.target.parentElement.classList.contains('delete-item')) {
-        console.log('delete imte');
-        e.target.parentElement.parentElement.remove();
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task');
     }
+
+    const li = document.createElement('li');
+    
+    li.className = 'collection-item';
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+
+    e.preventDefault();
 }
